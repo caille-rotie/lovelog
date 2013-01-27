0bef570a7fbd7c77f23992b82f18a1039d511dc7
Caille Rotie <caille_rotie@yahoo.fr>
2013-01-27 20:18:02 +0100

    gtalk début

--- src/main/java/fr/rotie/caille/lovelog/camel/LoveRoute.java
+++ src/main/java/fr/rotie/caille/lovelog/camel/LoveRoute.java
@@ -12,26 +12,38 @@ public class LoveRoute extends SpringRouteBuilder{
 	@Override
 	public void configure() throws Exception {
         
-        CastorDataFormat castor = new CastorDataFormat ();
-        castor.setMappingFile("castor-mapping.xml");
+        CastorDataFormat castorEmpathy = new CastorDataFormat ();
+        castorEmpathy.setMappingFile("castor-empathy-mapping.xml");
+        
+        CastorDataFormat castorGtalk = new CastorDataFormat ();
+        castorEmpathy.setMappingFile("castor-gtalk-mapping.xml");
     	
         from("file://io/input?noop=true")
             .log("file : ${file:name}")
             .setHeader("logFile", method("logMessageDao","getLogfile"))
             .log("header : ${headers.logFile}")
-            .doTry()
-            	.to("validator:empathy.xsd")
-            	.to("direct:empathy")
-            	
-            .doCatch(java.lang.Exception.class)
-	            .to("direct:emesene")
+            .choice()
+                .when(simple("${file:name} == 'schemas.xml'")).to("direct:other")
+                .when(simple("${file:name} == 'gtalk.xml'")).to("direct:other")
+            	.when(body().contains("google:archive:conversation")).to("direct:gtalk")
+            	.otherwise()
+		            .doTry()
+		            	.to("validator:empathy.xsd")
+		            	.to("direct:empathy")
+		            	
+		            .doCatch(java.lang.Exception.class)
+			            .to("direct:emesene")
 	        .end();
+        
+        from("direct:other")
+			.log("type : non-parsé")
+	    	.end();
         
         from("direct:empathy")
     		.log("type : empathy")
         	.split().tokenizeXML("message")
         	.log("ligne: ${body}")
-            .unmarshal(castor)
+            .unmarshal(castorEmpathy)
             .beanRef("empathyLogMessageDao", "parseInstant")
             .beanRef("logMessageDao", "attachLogFile(${body}, ${headers.logFile})")
             .beanRef("logMessageDao", "createLogMessage")
@@ -48,6 +60,17 @@ public class LoveRoute extends SpringRouteBuilder{
             .log("ligne: ${body}")
     		//                .to("jpa:fr.rotie.caille.lovelog.model.LogMessage")
             .end();
+        
+        from("direct:gtalk")
+    		.log("type : gtalk")
+        	.split().tokenizeXML("cli:message")
+        	.log("ligne: ${body}")
+//            .unmarshal(castorGtalk)
+//            .beanRef("empathyLogMessageDao", "parseInstant")
+//            .beanRef("logMessageDao", "attachLogFile(${body}, ${headers.logFile})")
+//            .beanRef("logMessageDao", "createLogMessage")
+        	.log("ligne: ${body}")
+        	.end();
 
     	
     	
